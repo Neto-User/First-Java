42d882e17553fb18ad80706d9e2ab35b3141a66e
Neto-User <netobox00@gmail.com>
2025-10-30 23:54:51 -0300

    push

---
+++ tipo-de-dados/TiposPrimitivos.java
@@ -0,0 +1,17 @@
+public class TiposPrimitivos{
+        public static void main(String[] args) {
+
+            // Tipos primitivos em Java
+            
+            int numeroInteiro = 10;          // Tipo inteiro 
+            double numeroDecimal = 10.5;     // Tipo ponto flutuante 
+            char caractere = 'A';            // Tipo caractere que é UMA SÓ LETRA e com aspas simples
+            boolean verdadeiroFalso = true;  // Tipo booleano
+
+            // Exibindo os valores
+            System.out.println("Número Inteiro: " + numeroInteiro);
+            System.out.println("Número Decimal: " + numeroDecimal);
+            System.out.println("Caractere: " + caractere);
+            System.out.println("Booleano: " + verdadeiroFalso);
+        }   
+} 
