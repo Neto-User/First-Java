f67c096251f0d68473b9a9b02e3d1510c4b4224f
Neto-User <netobox00@gmail.com>
2025-11-02 15:31:53 -0300

    Estrutura final corrigida: pom.xml e src/ na raiz do repositorio

---
+++ src/main/java/com/myjava/first/java/FirstJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myjava.first.java;
+
+/**
+ *
+ * @author n3to
+ */
+public class FirstJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
